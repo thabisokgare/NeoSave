ee2e50c3d0c29576ca6cc77cd11ae3637bc5a04d
thabisokgare <thabisoryan8@gmail.com>
2025-06-29 22:44:41 +0200

    Add sourcelink configuration for NeoSave.API to enable source linking in debugging

--- backend/NeoSave.API/Models/AppUser.cs
+++ backend/NeoSave.API/Models/AppUser.cs
@@ -8,9 +8,9 @@ namespace NeoSave.API.Models
 {
     public class AppUser : IdentityUser
     {
-        public int Id { get; set; }
+        public new string Id { get; set; }
         public string? Name { get; set; } = string.Empty;
-        public string? Email { get; set; } = string.Empty;
+        public new string Email { get; set; }
         public string? Password { get; set; }
 
         public List<Budget>? Budgets { get; set; } = new List<Budget>();
--- backend/NeoSave.API/obj/Debug/net9.0/NeoSave.API.AssemblyInfo.cs
+++ backend/NeoSave.API/obj/Debug/net9.0/NeoSave.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NeoSave.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12c1ed9809d2a8424b88609f38777db838aff2f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60d4fd09754be723228f2aee3481cfc1e73a0402")]
 [assembly: System.Reflection.AssemblyProductAttribute("NeoSave.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NeoSave.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
